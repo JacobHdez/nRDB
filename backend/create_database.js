88388fb1e54732bf43f878d5f7adde052e6d2f5a
Jacobo Hernandez Varela <48144152+JacobHdez@users.noreply.github.com>
2024-10-16 03:47:48 -0600

    feat: fetch coordinates and weather
    fetch all coordinates and weather for municipalities of Guanajuato in the five year history

--- backend/create_database.js
+++ backend/create_database.js
@@ -1,11 +1,79 @@
-import { municipalities } from "./data.js"
+import { MongoClient } from 'mongodb'
 
-const base_url_coordinates = 'http://api.openweathermap.org/geo/1.0/direct'
+import { municipalities } from './data.js'
 
-const fetch_coordinates = async (municipality) => {
-    const url = `${base_url_coordinates}?q=${municipality},Guanajuato,MX&appid=${process.env.OPENWEATHER_API_KEY}`
+
+const base_url_coord = 'https://geocoding-api.open-meteo.com/v1/search'
+const base_url_weather = 'https://archive-api.open-meteo.com/v1/archive'
+
+async function connect_to_mongodb( uri )
+{
+    let mongo_client
     try {
-        const response = await fetch(url)
+        mongo_client = new MongoClient( uri )
+        console.log(`Connecting to MongoDB ...`)
+
+        await mongo_client.connect()
+        console.log('Connected to MongoDB ...')
+
+        return mongo_client
+    }
+    catch ( error ) {
+        console.log('Connection to MongoDB failed:', error)
+    }
+}
+
+async function fetch_coordinates( municipality )
+{
+    const url = `${base_url_coord}?name=${municipality}&count=20&format=json&language=es`
+    try {
+        const response = await fetch( url )
+        if ( !response.ok ) {
+            throw new Error(`http error: ${response.status}`)
+        }
+        const data = await response.json()
+        for ( const result of data.results ) {
+            if ( result.admin1 === 'Guanajuato' ) {
+                if ( municipality === 'Santa Catarina' && result.admin2 === 'Xichú' )
+                    continue
+                return result
+            }
+        }
+    }
+    catch ( error ) {
+        throw error
+    }
+}
+
+async function get_coordinates()
+{
+    const db_uri = process.env.MONGODB_URI
+    let mongo_client
+    try {
+        mongo_client = await connect_to_mongodb( db_uri )
+        const db = mongo_client.db( 'weather' )
+        const collection = db.collection( 'guanajuato' )
+
+        for ( const municipality of municipalities ) {
+            const m_data = await fetch_coordinates( municipality )
+            const m_coord = {
+                name: m_data.name,
+                lat: m_data.latitude,
+                lon: m_data.longitude
+            }
+            await collection.insertOne( m_coord )
+        }
+    }
+    finally {
+        mongo_client.close()
+    }
+}
+
+async function fetch_weather( lat, lon, start_date, end_date )
+{
+    const url = `${base_url_weather}?latitude=${lat}&longitude=${lon}&start_date=${start_date}&end_date=${end_date}&hourly=temperature_2m,surface_pressure`
+    try {
+        const response = await fetch( url )
         if ( !response.ok ) {
             throw new Error(`http error: ${response.status}`)
         }
@@ -17,16 +85,37 @@ const fetch_coordinates = async (municipality) => {
     }
 }
 
-async function get_coordinates() {
-    const coordinates = []
-    for ( const municipality of municipalities ) {
-        const data = (await fetch_coordinates(municipality))[0]
-        const coord = {
-            name: data.name,
-            lat: data.lat,
-            lon: data.lon
+async function get_weather()
+{
+    const start_date = '2019-01-01'
+    const end_date = '2024-10-12'
+
+    const db_uri = process.env.MONGODB_URI
+    let mongo_client
+    try {
+        mongo_client = await connect_to_mongodb( db_uri )
+        const db = mongo_client.db( 'weather' )
+        const collection = db.collection( 'guanajuato' )
+
+        const c_gto = await collection.find().toArray()
+        var count = 0
+        for ( const d_m of c_gto ) {
+            console.log( `[${++count}] ${d_m.name} => ${d_m.lat},${d_m.lon}` )
+            
+            const data = await fetch_weather( d_m.lat, d_m.lon, start_date, end_date )
+
+            const result = await collection.updateOne(
+                { name: d_m.name },
+                { $set: {
+                    hourly_units: data.hourly_units,
+                    hourly: data.hourly
+                } }
+            )
+
+            console.log(`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`)
         }
-        coordinates.push(coord)
     }
-    console.log(coordinates)
+    finally {
+        mongo_client.close()
+    }
 }
