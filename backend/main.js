e088635bfd46409753ec17b7c22c318f6313064f
Jacobo Hernandez Varela <48144152+JacobHdez@users.noreply.github.com>
2024-11-12 00:43:01 -0600

    feat: filter data by date

--- backend/main.js
+++ backend/main.js
@@ -26,9 +26,43 @@ const server = http.createServer( async ( request, response ) => {
         return
     }
 
+    var start_date = request_url.searchParams.get( 'start_date' )
+    if ( !start_date ) start_date = data.hourly.time.at(0)
+    var end_date = request_url.searchParams.get( 'end_date' )
+    if ( !end_date ) end_date = data.hourly.time.at(-1)
+
+    const filtered_data = filter_data( data, start_date, end_date )
+
     response.writeHead( 200, { 'Content-Type': 'application/json' } )
-    response.end( JSON.stringify( { data: data } ) )
+    response.end( JSON.stringify( { data: filtered_data } ) )
 } )
 server.listen( port, hostname, () => {
     console.log( `Server is running on http://${hostname}:${port}` )
 } )
+
+function filter_data( data, start_date, end_date )
+{
+    const startDate = new Date( start_date )
+    const endDate = new Date( end_date )
+
+    const filtered_data = {
+        ...data,
+        hourly: {
+            time: [],
+            temperature_2m: [],
+            surface_pressure: []
+        }
+    }
+
+    for ( let i = 0; i < data.hourly.time.length; ++i ) {
+        const timestamp = new Date( data.hourly.time[i] )
+
+        if ( timestamp >= startDate && timestamp <= endDate ) {
+            filtered_data.hourly.time.push( data.hourly.time[i] )
+            filtered_data.hourly.temperature_2m.push( data.hourly.temperature_2m[i] )
+            filtered_data.hourly.surface_pressure.push( data.hourly.surface_pressure[i] )
+        }
+    }
+
+    return filtered_data
+}
